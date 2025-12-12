0f11e9db3abb194a53d949db7c6cfc3582ec7798
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-12 07:35:10 +0300

    Proje dosyası ekle.

---
+++ FlashMediator/src/FlashMediator.Contracts/IBaseRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashMediator.src.FlashMediator.Contracts
+{
+    public interface IBaseRequest
+    {
+    }
+    public interface IRequest<TResponse> : IBaseRequest
+    {
+    }
+    public interface IRequest : IBaseRequest
+    {
+    }
+}
---
+++ FlashMediator/src/FlashMediator.Contracts/IRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashMediator.src.FlashMediator.Contracts
+{
+    public interface IRequestHandler <in TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        Task<TResponse> Handle(TRequest request,CancellationToken cancellationToken);
+    }
+    public interface IRequestHandler <in TRequest>
+        where TRequest : IRequest
+    {
+        Task Handle(TRequest request,CancellationToken cancellationToken);
+    }
+}
